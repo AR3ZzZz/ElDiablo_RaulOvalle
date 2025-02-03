a94e859ad83b129ff56b4afb054f14edfe10baed
DESKTOP-GV16N45\Alumno <informatica@esne.es>
2025-02-03 21:08:44 +0100

    WallDetector

---
+++ Assets/Scripts/WallDetector.cs
@@ -0,0 +1,25 @@
+using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallDetector : MonoBehaviour
+{
+    [SerializeField] private float colorChange;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Obstacle"))
+        {
+            Material wallMaterial = other.GetComponent<MeshRenderer>().material;
+            Color tranparency = new Color(wallMaterial.color.r, wallMaterial.color.g, wallMaterial.color.b, .25f);
+            wallMaterial.DOColor(tranparency, colorChange);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Material wallMaterial = other.GetComponent<MeshRenderer>().material;
+        Color opaque = new Color(wallMaterial.color.r, wallMaterial.color.g, wallMaterial.color.b, 1f);
+        wallMaterial.DOColor(opaque, colorChange);
+    }
+}
